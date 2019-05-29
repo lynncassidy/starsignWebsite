01a9f83d20bb1bcc0f6d7fa65ed80ea0e42e4e90
lynnC <lynncassidy91@gmail.com>
2019-05-29 14:52:46 +0100

    initial commit

---
+++ scripts.js
@@ -0,0 +1,101 @@
+function onHover(elementHovered)
+{
+
+    if(elementHovered == document.getElementById("ariesImage")){
+        elementHovered.setAttribute("src","imagesStarsign/aries.jpg");
+    }
+    else if(elementHovered == document.getElementById("taurusImage")){
+        elementHovered.setAttribute("src","imagesStarsign/taurus.jpg");
+    }
+    else if(elementHovered == document.getElementById("geminiImage")){
+        elementHovered.setAttribute("src","imagesStarsign/gemini.jpg");
+    }
+    else if(elementHovered == document.getElementById("cancerImage")){
+        elementHovered.setAttribute("src","imagesStarsign/cancer.jpg");
+    }
+    else if(elementHovered == document.getElementById("leoImage")){
+        elementHovered.setAttribute("src","imagesStarsign/leo.jpg");
+    }
+    else if(elementHovered == document.getElementById("virgoImage")){
+        elementHovered.setAttribute("src","imagesStarsign/virgo.jpg");
+    }
+    else if(elementHovered == document.getElementById("libraImage")){
+        elementHovered.setAttribute("src","imagesStarsign/libra.jpg");
+    }
+    else if(elementHovered == document.getElementById("piscesImage")){
+        elementHovered.setAttribute("src","imagesStarsign/Pisces.jpg");
+    }
+    else if(elementHovered == document.getElementById("saggitariusImage")){
+        elementHovered.setAttribute("src","imagesStarsign/saggitarius.jpg");
+    }
+    else if(elementHovered == document.getElementById("scorpioImage")){
+        elementHovered.setAttribute("src","imagesStarsign/scorpio.jpg");
+    }
+    else if(elementHovered == document.getElementById("capricornImage")){
+        elementHovered.setAttribute("src","imagesStarsign/capricorn.jpg");
+    }
+    else if(elementHovered == document.getElementById("aquariusImage")){
+        elementHovered.setAttribute("src","imagesStarsign/aquarius.jpg");
+    }
+    
+
+    
+}
+
+function offHover(elementOffHover)
+{   
+    if(elementOffHover == document.getElementById("ariesImage")){
+        elementOffHover.setAttribute("src","imagesStarsign/ariesConstellation.jpg");
+    }
+    else if(elementOffHover == document.getElementById("taurusImage")){
+        elementOffHover.setAttribute("src","imagesStarsign/taurusBull.jpg");
+    }
+    else if(elementOffHover == document.getElementById("geminiImage")){
+        elementOffHover.setAttribute("src","imagesStarsign/geminiTwins.jpg");
+    }
+    else if(elementOffHover == document.getElementById("cancerImage")){
+        elementOffHover.setAttribute("src","imagesStarsign/cancerCrab.jpg");
+    }
+    else if(elementOffHover == document.getElementById("leoImage")){
+        elementOffHover.setAttribute("src","imagesStarsign/leoLion.jpg");
+    }
+    else if(elementOffHover == document.getElementById("virgoImage")){
+        elementOffHover.setAttribute("src","imagesStarsign/virgoWoman.jpg");
+    }
+    else if(elementOffHover == document.getElementById("libraImage")){
+        elementOffHover.setAttribute("src","imagesStarsign/libraScales.jpg");
+    }
+    else if(elementOffHover == document.getElementById("piscesImage")){
+        elementOffHover.setAttribute("src","imagesStarsign/PiscesFish.jpg");
+    }
+    else if(elementOffHover == document.getElementById("saggitariusImage")){
+        elementOffHover.setAttribute("src","imagesStarsign/saggitariusArcher.jpg");
+    }
+    else if(elementOffHover == document.getElementById("scorpioImage")){
+        elementOffHover.setAttribute("src","imagesStarsign/scorpioScorpion.jpg");
+    }
+    else if(elementOffHover == document.getElementById("capricornImage")){
+        elementOffHover.setAttribute("src","imagesStarsign/capricornGoat.jpg");
+    }
+    else if(elementOffHover == document.getElementById("aquariusImage")){
+        elementOffHover.setAttribute("src","imagesStarsign/aquariusMan.jpg");
+    }
+
+
+
+    /*document.getElementById("taurusImage").setAttribute("src","imagesStarsign/taurusBull.jpg");
+    //$("ariesImage").attr('scr',"imagesStarsign/aries.jpg");
+    
+    document.getElementById("ariesImage").setAttribute("src","imagesStarsign/ariesConstellation.jpg");
+    document.getElementById("geminiImage").setAttribute("src","imagesStarsign/geminiTwins.jpg");
+    document.getElementById("cancerImage").setAttribute("src","imagesStarsign/cancerCrab.jpg");
+    document.getElementById("leoImage").setAttribute("src","imagesStarsign/leoLion.jpg");
+    document.getElementById("virgoImage").setAttribute("src","imagesStarsign/virgoWoman.jpg");
+    document.getElementById("libraImage").setAttribute("src","imagesStarsign/libraScales.jpg");
+    document.getElementById("piscesImage").setAttribute("src","imagesStarsign/piscesFish.jpg");
+    document.getElementById("saggitariusImage").setAttribute("src","imagesStarsign/saggitariusArcher.jpg");
+    document.getElementById("scorpioImage").setAttribute("src","imagesStarsign/scorpioScorpion.jpg");
+    document.getElementById("capricornImage").setAttribute("src","imagesStarsign/capricornGoat.jpg");
+    document.getElementById("aquariusImage").setAttribute("src","imagesStarsign/aquariusMan.jpg");*/
+
+}
